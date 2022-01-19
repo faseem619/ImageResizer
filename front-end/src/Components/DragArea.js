603a802477bbb3476657ee14cf6b8aed921e823f
faseem619 <faseem619@gmail.com>
2022-01-19 14:44:24 +0530

    added more components to drag area

--- front-end/src/Components/DragArea.js
+++ front-end/src/Components/DragArea.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "../CSS/dragarea.css";
+import SelectButton from "./SelectButton";
 function DragArea() {
   return (
     <div className="drag_area--container">
-      <div className="drag_area"></div>
+      <div className="drag_area">
+        <i className="fas fa-image"></i>
+        <p>Drop Your Image Here!</p>
+        <SelectButton />
+      </div>
     </div>
   );
 }
---
+++ front-end/src/Components/SelectButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../CSS/button.css";
+
+function SelectButton() {
+  return <div className=" button button--select-file">Select Image</div>;
+}
+
+export default SelectButton;
