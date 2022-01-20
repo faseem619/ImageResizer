297b237f2360b9cbb04026634805eb772ff1db3d
faseem619 <faseem619@gmail.com>
2022-01-20 11:30:40 +0530

    added CORS to backend

--- backend/src/main/java/com/backend/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/backend/BackendApplication.java
@@ -26,7 +26,7 @@ public class BackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
