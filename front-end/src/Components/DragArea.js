65ddcf17fb0d6d49ba91e5128415b2d83111626b
faseem619 <faseem619@gmail.com>
2022-01-25 17:17:15 +0530

    changed images state to single image instead of array

--- front-end/src/Components/DragArea.js
+++ front-end/src/Components/DragArea.js
@@ -7,7 +7,7 @@ import SelectButton from "./SelectButton";
 function DragArea({ setHeight, setWidth, setFile, file, setId }) {
   const dragArea = useRef(null);
 
-  let [images, setImages] = useState([]);
+  let [images, setImages] = useState("");
 
   //sets file state to dropped image
   //removes the highlighted effect when hovering over with file
@@ -32,7 +32,7 @@ function DragArea({ setHeight, setWidth, setFile, file, setId }) {
     let reader = new FileReader();
     reader.readAsDataURL(myFile);
     reader.onload = function () {
-      setImages([...images, reader.result]);
+      setImages(reader.result);
       let image = new Image();
       image.src = reader.result;
       image.onload = () => {
@@ -59,14 +59,13 @@ function DragArea({ setHeight, setWidth, setFile, file, setId }) {
         onDragLeave={handleDragLeave}
         ref={dragArea}
       >
-        {images.map((imageUrl) => (
+        {images && (
           <img
-            src={imageUrl}
+            src={images}
             alt="file failed to load"
             className="preview-image"
-            key={imageUrl.toString()}
           ></img>
-        ))}
+        )}
         <i className="fas fa-image"></i>
         <p>Drop Your Image Here!</p>
         <SelectButton updateImage={updateImage} setFile={setFile} />
