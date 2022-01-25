df1d34ed5ddae7811dad39a19d56c69264b94203
faseem619 <faseem619@gmail.com>
2022-01-25 23:58:55 +0530

    gave buttons click effects and made github icon redirect to repo

--- front-end/src/Components/Navbar.js
+++ front-end/src/Components/Navbar.js
@@ -4,7 +4,12 @@ function Navbar() {
   return (
     <div className="navbar">
       <h1>Image Resizer</h1>
-      <i className="fab fa-github-square"></i>
+      <i
+        className="fab fa-github-square"
+        onClick={() =>
+          (window.location.href = "https://github.com/faseem619/ImageResizer")
+        }
+      ></i>
     </div>
   );
 }
