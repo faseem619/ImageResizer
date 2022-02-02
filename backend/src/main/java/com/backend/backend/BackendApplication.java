20cfd82e3bd0bf19d0f0f306b5830617a52b768f
faseem619 <faseem619@gmail.com>
2022-02-02 22:05:47 +0530

    checking image type

--- backend/src/main/java/com/backend/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/backend/BackendApplication.java
@@ -122,7 +122,7 @@ public class BackendApplication {
 	}
 	
   public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
-    BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
     Graphics2D graphics2D = resizedImage.createGraphics();
     graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
     graphics2D.dispose();
